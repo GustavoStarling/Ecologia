2968f1f8eefab40bff07fb0ee3410cc44f8b4c8a
Kemilyy <133906744+Kemilyy@users.noreply.github.com>
2023-06-19 13:59:28 -0300

    Delete Ecologia directory

--- Ecologia/js/script.js
+++
@@ -1,6 +0,0 @@
-const hamburguerEl = document.querySelector('#hamburguer');
-const navEl = document.querySelector('nav');
-
-hamburguerEl.addEventListener('click', function () {
-    navEl.classList.toggle('ativo');
-});
